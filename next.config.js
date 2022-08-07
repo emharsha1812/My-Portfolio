19c24e091b8f5b3e9663981950029fd29b189937
Harshwardhan <harshafartale12@gmail.com>
2022-08-07 17:31:26 +0530

    PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+
   i18n: {
-    locales: ['en'], // Add your languages here
-    defaultLocale: 'en',
+    locales: ["en"], // Add your languages here
+    defaultLocale: "en",
     localeDetection: false,
   },
-}
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,45 @@
-import { useContext } from 'react'
+import { useContext } from "react";
 import {
   ReactBricksContext,
   PageViewer,
   fetchPage,
   cleanPage,
   types,
-} from 'react-bricks/frontend'
-import Head from 'next/head'
-import { GetStaticProps } from 'next'
+} from "react-bricks/frontend";
+import Head from "next/head";
+import { GetStaticProps } from "next";
 
-import config from '../react-bricks/config'
-import Layout from '../components/layout'
-import ErrorNoKeys from '../components/errorNoKeys'
-import ErrorNoHomePage from '../components/errorNoHomePage'
+import config from "../react-bricks/config";
+import Layout from "../components/layout";
+import ErrorNoKeys from "../components/errorNoKeys";
+import ErrorNoHomePage from "../components/errorNoHomePage";
 
 interface HomeProps {
-  page: types.Page
-  error: string
+  page: types.Page;
+  error: string;
 }
 
 const Home: React.FC<HomeProps> = ({ page, error }) => {
+  <Head>
+    <meta charSet="utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+    />
+    <meta name="description" content="description of your project" />
+    <meta name="theme-color" content="#000" />
+    <title>Portfolio</title>
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+  </Head>;
+
   // Clean the received content
   // Removes unknown or not allowed bricks
-  const { pageTypes, bricks } = useContext(ReactBricksContext)
+  const { pageTypes, bricks } = useContext(ReactBricksContext);
 
-  const pageOk = page ? cleanPage(page, pageTypes, bricks) : null
+  const pageOk = page ? cleanPage(page, pageTypes, bricks) : null;
 
   return (
     <Layout>
@@ -37,22 +52,22 @@ const Home: React.FC<HomeProps> = ({ page, error }) => {
           <PageViewer page={pageOk} />
         </>
       )}
-      {error === 'NOKEYS' && <ErrorNoKeys />}
-      {error === 'NOPAGE' && <ErrorNoHomePage />}
+      {error === "NOKEYS" && <ErrorNoKeys />}
+      {error === "NOPAGE" && <ErrorNoHomePage />}
     </Layout>
-  )
-}
+  );
+};
 
 export const getStaticProps: GetStaticProps = async (context) => {
   if (!config.apiKey) {
-    return { props: { error: 'NOKEYS' } }
+    return { props: { error: "NOKEYS" } };
   }
   try {
-    const page = await fetchPage('home', config.apiKey, context.locale)
-    return { props: { page } }
+    const page = await fetchPage("home", config.apiKey, context.locale);
+    return { props: { page } };
   } catch {
-    return { props: { error: 'NOPAGE' } }
+    return { props: { error: "NOPAGE" } };
   }
-}
+};
 
-export default Home
+export default Home;
